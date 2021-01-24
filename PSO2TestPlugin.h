5c1ddc589c954daa3118ffb726b5121dad163a8b
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-01-23 16:43:23 -0800

    Make this one line

--- PSO2TestPlugin.h
+++ PSO2TestPlugin.h
@@ -12,13 +12,7 @@ namespace PSO2TestPlugin {
 BOOL WINAPI DllMain(HINSTANCE hInst, DWORD fdwReason, LPVOID) {
     if (fdwReason == DLL_PROCESS_ATTACH) {
         DisableThreadLibraryCalls(hInst);
-        CreateThread(
-                nullptr,
-                0,
-                reinterpret_cast<LPTHREAD_START_ROUTINE>(&PSO2TestPlugin::Initialize),
-                nullptr,
-                0,
-                nullptr);
+        CreateThread(nullptr, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(&PSO2TestPlugin::Initialize), nullptr, 0, nullptr);
     }
     return TRUE;
 }
