e9bb3c11822acdd9406eb9ccb7d55ee3242dc4b0
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-01-25 18:41:11 -0800

    Add VS2019 Enterprise build support

--- BuildDetours.bat
+++ BuildDetours.bat
@@ -1,3 +1,4 @@
 @echo off
 @call "%programfiles(x86)%\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
+@call "%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat"
 nmake
