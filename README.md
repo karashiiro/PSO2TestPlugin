be7b126a866b5a3a223b9e5c1c0105b1f3a5e4e8
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-03-17 16:15:02 -0700

    Update dependencies to x64 versions

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # PSO2TestPlugin
-Build notes: Visual Studio 2019 x86 required. Use the generator `"Visual Studio 16 2019"`
-with the target architecture `Win32` (PSO2 runs in WOW64 mode on 64-bit machines).
--- scripts/BuildDetours.bat
+++ scripts/BuildDetours.bat
@@ -1,4 +1,4 @@
 @echo off
-@call "%programfiles(x86)%\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
-@call "%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat"
+@call "%programfiles(x86)%\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat" -host_arch=amd64
+@call "%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat" -host_arch=amd64
 nmake
